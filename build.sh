6c45d0945e6d4417740b443cf5a4e4bb55263d2b
tntudor <tudor.negrescu@gmail.com>
2015-09-09 18:35:26 +0300

    Add remove .box files in build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 name=`cat box-name.txt`
 version=`cat Version`
 
+rm -f *.box
+
 echo "Building box ${name} version: ${version} ..."
 vagrant package --output "${name}-v.${version}.box"
 vagrant destroy -f
